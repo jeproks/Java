a0b786d88460c71f8ea1304788ae314801ae243a
Jenlee Jeff <jenleejeffbriones24@gmail.com>
2016-06-20 11:11:52 +0800

    Update Comparison.java
    Last

--- Comparison.java
+++ Comparison.java
@@ -2,7 +2,7 @@ import java.util.Scanner; // program uses class Scanner
 public class Comparison  
 { 
     // main method begins execution of Java application    
-    public static void main( String args[] )    
+    public static void main( String [] args )  {  
     {
         // create Scanner to obtain input from command window       
         Scanner input = Scanner( System.in );  
@@ -15,7 +15,7 @@ public class Comparison
         System.out.print( "Enter second integer: " ); // prompt        
         number2 = input.nextInt(); // read second number from user               
         
-        if ( number1 == number2 )           
+        if ( number1 = number2 )           
             System.out.printf( "%d == %d\n", number1, number2 );  
         if ( number1 != number2 )          
             System.out.printf( "%d != %d\n", number1, number2 );  
